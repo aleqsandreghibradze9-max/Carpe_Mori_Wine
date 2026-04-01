9c3efe07e29080c804ec95e4470ddb22ab6715f0
aleqsandreghibradze9-max <aleqsandreghibradze9@gmail.com>
2026-04-01 22:50:03 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,19 @@
-const CACHE = "sommelier-1773163031";
-const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE = "sommelier-v1775069105";
 
-// Install - cache all assets
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting(); // activate immediately without waiting
+  self.skipWaiting();
 });
 
-// Activate - delete ALL old caches automatically
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
-  self.clients.claim(); // take control of all open tabs immediately
+  self.clients.claim();
 });
 
-// Fetch - network first, fall back to cache
-// This means users ALWAYS get the latest version if online
+// Always fetch fresh - no caching at all
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // Update cache with fresh response
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request)) // offline fallback
-  );
+  e.respondWith(fetch(e.request));
 });
